785ef466b8e1afed768e61b6ce00046a0ed41902
olga.ilyina <olga.ilyina@axenix.pro>
2024-11-07 14:18:16 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/collection/ListUtils.java
@@ -0,0 +1,64 @@
+package collection;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index + 1) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            if (!Stream.of(list.get(iterator.previousIndex())).filter(filter).toList().isEmpty()) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            if (!Stream.of(list.get(iterator.previousIndex())).filter(filter).toList().isEmpty()) {
+                iterator.set(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            if (elements.contains(list.get(iterator.previousIndex()))) {
+                iterator.remove();
+            }
+        }
+    }
+}
---
+++ src/test/java/collection/ListUtilsTest.java
@@ -0,0 +1,136 @@
+package collection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ListUtilsTest {
+
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenRemoveAllNotEmpyRemoveFirst() {
+        ListUtils.removeAll(input, List.of(1));
+        assertThat(input).hasSize(1).containsSequence(3);
+    }
+
+    @Test
+    void whenRemoveAllNotEmpyRemoveLast() {
+        ListUtils.removeAll(input, List.of(3));
+        assertThat(input).hasSize(1).containsSequence(1);
+    }
+
+    @Test
+    void whenRemoveAllNotEmpyRemoveAll() {
+        ListUtils.removeAll(input, List.of(3, 1));
+        assertThat(input).hasSize(0);
+        assertThat(input).isEmpty();
+    }
+
+    @Test
+    void whenRemoveAllNotEmpyRemoveNotElements() {
+        ListUtils.removeAll(input, List.of(7, 10));
+        assertThat(input).hasSize(2).containsSequence(1, 3);
+    }
+
+    @Test
+    void whenRemoveAllEmpy() {
+        input = new ArrayList<>();
+        ListUtils.removeAll(input, List.of(3, 1));
+        assertThat(input).hasSize(0);
+        assertThat(input).isEmpty();
+    }
+
+    @Test
+    void whenRemoveIfFirst() {
+        ListUtils.removeIf(input, x -> x < x * x);
+        assertThat(input).hasSize(1).containsSequence(1);
+    }
+
+    @Test
+    void whenRemoveIfLast() {
+        ListUtils.removeIf(input, x -> x == x * x);
+        assertThat(input).hasSize(1).containsSequence(3);
+    }
+
+    @Test
+    void whenRemoveIfAll() {
+        ListUtils.removeIf(input, x -> x <= x * x);
+        assertThat(input).hasSize(0);
+        assertThat(input).isEmpty();
+    }
+
+    @Test
+    void whenRemoveIfNot() {
+        ListUtils.removeIf(input, x -> x > x * x);
+        assertThat(input).hasSize(2).containsSequence(1, 3);
+    }
+
+    @Test
+    void whenRemoveIfEmpty() {
+        input = new ArrayList<>();
+        ListUtils.removeIf(input, x -> x.equals(x));
+        assertThat(input).hasSize(0);
+        assertThat(input).isEmpty();
+    }
+
+    @Test
+    void whenReplaceIfFirst() {
+        ListUtils.replaceIf(input, x -> x < x * x, 7);
+        assertThat(input).hasSize(2).containsSequence(1, 7);
+    }
+
+    @Test
+    void whenReplaceIfLast() {
+        ListUtils.replaceIf(input, x -> x == x * x, 7);
+        assertThat(input).hasSize(2).containsSequence(7, 3);
+    }
+
+    @Test
+    void whenReplaceeIfAll() {
+        ListUtils.replaceIf(input, x -> x <= x * x, 7);
+        assertThat(input).hasSize(2).containsSequence(7, 7);
+    }
+
+    @Test
+    void whenReplaceIfNot() {
+        ListUtils.replaceIf(input, x -> x > x * x, 7);
+        assertThat(input).hasSize(2).containsSequence(1, 3);
+    }
+
+    @Test
+    void whenReplaceIfEmpty() {
+        input = new ArrayList<>();
+        ListUtils.replaceIf(input, x -> x.equals(x), 7);
+        assertThat(input).hasSize(0);
+        assertThat(input).isEmpty();
+    }
+}
