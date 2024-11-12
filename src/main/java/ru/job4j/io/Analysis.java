ca5901d1a1d810a7fc3a962b6751063e58d97fad
olga.ilyina <olga.ilyina@axenix.pro>
2024-11-12 22:22:45 +0300

    7. Анализ доступности сервера [#859 #521433]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,76 +1,35 @@
 package ru.job4j.io;
 
-import java.io.*;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> results = getUnavailableList(getSortedByLocalTimeList(source));
-        try (PrintWriter output = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            for (String result : results) {
-                output.write(result + System.lineSeparator());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<Map.Entry<LocalTime, String>> getSortedByLocalTimeList(String source) {
-        List<Map.Entry<LocalTime, String>> valueList = Collections.emptyList();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            List<String> list = reader.lines().toList();
-            DateTimeFormatter dfm = DateTimeFormatter.ofPattern("HH:mm:ss");
-            Map<LocalTime, String> values =
-                    list.stream()
-                            .map(e -> e.trim())
-                            .map(e -> e.split(" ", 2))
-                            .collect(Collectors.toMap(e -> LocalTime.parse(e[1], dfm), e -> e[0]))
-                            .entrySet()
-                            .stream()
-                            .sorted(Map.Entry.comparingByKey())
-                            .collect(Collectors.toMap(
-                                    Map.Entry::getKey,
-                                    Map.Entry::getValue,
-                                    (e1, e2) -> e1,
-                                    LinkedHashMap::new));
-            valueList = values.entrySet().stream().toList();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return valueList;
-    }
-
-    private List<String> getUnavailableList(List<Map.Entry<LocalTime, String>> valueList) {
-        List<String> results = new ArrayList<>();
-        int i = 0;
-        while (i < valueList.size()) {
+        try (BufferedReader input = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(target)) {
             String start = "";
             String end = "";
-            while (i < valueList.size() && (!valueList.get(i).getValue().equals("500") && !valueList.get(i).getValue().equals("400"))) {
-                i++;
+            for (String line = input.readLine(); line != null; line = input.readLine()) {
+                String[] elements = line.split(" ");
+                if ("400".equals(elements[0]) || "500".equals(elements[0])) {
+                    start = "".equals(start) ? elements[1] : start;
+                }
+                if (!"400".equals(elements[0]) && !"500".equals(elements[0])) {
+                    end = "".equals(start) ? "" : elements[1];
+                }
+                if (!"".equals(start) && !"".equals(end)) {
+                    String s = start + ";" + end + System.lineSeparator();
+                    writer.println(s);
+                    System.out.println(s);
+                    start = "";
+                    end = "";
+                }
             }
-            if (i < valueList.size()) {
-                start = valueList.get(i).getKey().toString();
-                i++;
-            }
-            while (i < valueList.size() && (valueList.get(i).getValue().equals("500") || valueList.get(i).getValue().equals("400"))) {
-                i++;
-            }
-            if (i < valueList.size()) {
-                end = valueList.get(i).getKey().toString();
-                i++;
-            }
-            if (!start.equals("")) {
-                results.add(String.format("%s%s%s", start, ";", end));
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return results;
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -13,8 +13,10 @@ class AnalysisTest {
     void whenNotDataThenNotLine(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         File target = tempDir.resolve("target.txt").toFile();
-        Analysis analisis = new Analysis();
-        analisis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (PrintWriter output = new PrintWriter(source)) {
+        }
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
         try (BufferedReader input = new BufferedReader(new FileReader(target))) {
             input.lines().forEach(result::append);
@@ -26,14 +28,14 @@ class AnalysisTest {
     void whenNotUnvialableThenNotLine(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("300 11:02:02");
             output.println("202 09:28:13");
             output.println("200 10:04:17");
             output.println("305 10:04:18");
+            output.println("300 11:02:02");
         }
         File target = tempDir.resolve("target.txt").toFile();
-        Analysis analisis = new Analysis();
-        analisis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
         try (BufferedReader input = new BufferedReader(new FileReader(target))) {
             input.lines().forEach(result::append);
@@ -45,14 +47,14 @@ class AnalysisTest {
     void whenNotAvialableThenNotLine(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("500 11:02:02");
             output.println("400 09:28:13");
             output.println("400 10:04:17");
             output.println("500 10:04:18");
+            output.println("500 11:02:02");
         }
         File target = tempDir.resolve("target.txt").toFile();
-        Analysis analisis = new Analysis();
-        analisis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
         try (BufferedReader input = new BufferedReader(new FileReader(target))) {
             input.lines().forEach(result::append);
@@ -64,14 +66,14 @@ class AnalysisTest {
     void whenFirstAvialableThenOneLine(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("300 11:02:02");
             output.println("202 09:28:13");
             output.println("400 10:04:17");
             output.println("500 10:04:18");
+            output.println("300 11:02:02");
         }
         File target = tempDir.resolve("target.txt").toFile();
-        Analysis analisis = new Analysis();
-        analisis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
         try (BufferedReader input = new BufferedReader(new FileReader(target))) {
             input.lines().forEach(result::append);
@@ -83,14 +85,14 @@ class AnalysisTest {
     void whenFirstUnavialableThenOneLine(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("300 11:02:02");
             output.println("500 09:28:13");
             output.println("400 10:04:17");
             output.println("500 10:04:18");
+            output.println("300 11:02:02");
         }
         File target = tempDir.resolve("target.txt").toFile();
-        Analysis analisis = new Analysis();
-        analisis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
         try (BufferedReader input = new BufferedReader(new FileReader(target))) {
             input.lines().forEach(result::append);
@@ -102,16 +104,16 @@ class AnalysisTest {
     void whenTwoPeriodsThenTwoLine(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter output = new PrintWriter(source)) {
-            output.println("300 11:02:02");
             output.println("500 09:28:13");
-            output.println("500 11:31:08");
             output.println("400 10:04:17");
-            output.println("200 11:31:09");
             output.println("500 10:04:18");
+            output.println("300 11:02:02");
+            output.println("500 11:31:08");
+            output.println("200 11:31:09");
         }
         File target = tempDir.resolve("target.txt").toFile();
-        Analysis analisis = new Analysis();
-        analisis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder result = new StringBuilder();
         try (BufferedReader input = new BufferedReader(new FileReader(target))) {
             input.lines().map(e -> e + System.lineSeparator()).forEach(result::append);
