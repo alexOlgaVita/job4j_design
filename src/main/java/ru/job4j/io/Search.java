22b329bbceb7f337509c7d697fe347cd23955508
olga.ilyina <olga.ilyina@axenix.pro>
2024-11-14 09:16:03 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,30 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles();
         Files.walkFileTree(root, searcher);
         return searcher.getPaths().stream().filter(condition).toList();
+    }
+
+    private static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension is null. Please enter a value.");
+        }
+        Path path = Paths.get(args[0]);
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Директория не существует: %s", path.toFile().getAbsoluteFile()));
+        }
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Это не директория: %s", path.toFile().getAbsoluteFile()));
+        }
     }
 }
