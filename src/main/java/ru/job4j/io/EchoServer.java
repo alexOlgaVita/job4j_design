2fccb8608e7265dec0527bf354baf7728d1b6efc
olga.ilyina <olga.ilyina@axenix.pro>
2024-11-17 09:20:43 +0300

    3. Бот [#7921 #522192]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -9,8 +9,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EchoServer {
     private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
@@ -22,18 +20,18 @@ public class EchoServer {
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    List<String> response = new ArrayList<>();
+                    String msg = "";
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
-                        response.add(string);
+                        msg = "".equals(msg) ? string : msg;
                     }
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (response.stream().filter(e -> e.contains("?msg=Hello ")).count() == 1) {
+                    if (msg.contains("?msg=Hello ")) {
                         output.write("Hello".getBytes());
-                    } else if (response.stream().filter(e -> e.contains("?msg=Exit")).count() == 1) {
+                    } else if (msg.contains("?msg=Exit")) {
                         output.write("Завершить работу сервера".getBytes());
                         server.close();
-                    } else if (response.stream().filter(e -> e.contains("?msg=Any")).count() == 1) {
+                    } else if (msg.contains("?msg=Any")) {
                         output.write("What".getBytes());
                     }
                     output.flush();
