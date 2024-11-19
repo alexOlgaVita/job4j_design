1db33d1f2f5de1a6fa6dd1e928eba759f3b94b01
olga.ilyina <olga.ilyina@axenix.pro>
2024-11-19 21:32:07 +0300

    3. Фильтры [#1732]

---
+++ db/filters/insert.sql
@@ -0,0 +1,72 @@
+insert into type(name) 
+values ('СЫР'), ('МОЛОКО'), ('ХЛЕБОБУЛОЧНЫЕ ИЗДЕЛИЯ'), ('КИСЛОМОЛОЧНЫЕ ПРОДУКТЫ'),('РЫБА'), ('КОНДИТЕРСКИЕ ИЗДЕЛИЯ'), ('МЯСО');
+
+insert into product(name, type_id, expired_date, price) 
+values ('Плавленный сыр Волна', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Плавленный сыр Просто вкусно', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр пармезан', 1, '2024-11-30', 500.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр копченый', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр сулугуни', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр адыгейский', 1, '2024-11-30', 150.68);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр Российский', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр Голландский', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Колбасный сыр', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сырные палочки', 1, '2024-11-30', 120.54);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сыр маасдам', 1, '2024-11-30', 420.29);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Молоко пастеризоавнное', 2, '2024-11-30', 91.99)
+
+insert into product(name, type_id, expired_date, price) 
+values ('Молоко топленое', 2, '2024-11-30', 100.23);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Молоко соевое', 2, '2024-11-30', 121.99)
+
+insert into product(name, type_id, expired_date, price) 
+values ('Мороженое', 2, '2024-11-13', 67.20);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Батон', 3, '2024-11-30', 45.35);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Сметана', 4, '2024-11-30', 91.30);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Мороженый минтай', 5, '2024-11-30', 150.37);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Шоколад', 6, '2024-11-01', 82.17);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Курица', 7, '2024-11-03', 200.51);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Мороженая куриная печень', 7, '2024-11-30', 150.37);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Мороженое мясо', 7, '2024-11-30', 250.76);
+
+insert into product(name, type_id, expired_date, price) 
+values ('Охлажденная семга', 5, '2024-11-30', 500.54);
+
