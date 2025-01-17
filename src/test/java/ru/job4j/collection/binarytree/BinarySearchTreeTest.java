908932e8685bf7297de2ffc5672057b809ce83ba
olga.ilyina <olga.ilyina@axenix.pro>
2025-01-17 16:28:38 +0300

    Бинарное дерево поиска. Бинарное дерево поиска. Удаление узла. [#504985]

--- src/test/java/ru/job4j/collection/binarytree/BinarySearchTreeTest.java
+++ src/test/java/ru/job4j/collection/binarytree/BinarySearchTreeTest.java
@@ -107,4 +107,59 @@ class BinarySearchTreeTest {
         assertThat(tree.inPostOrder()).hasSize(7)
                 .containsExactly(1, 3, 2, 5, 7, 6, 4);
     }
+
+    @Test
+    void whenRemoveRootWithoutChildsThenOk() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int element : new int[]{4}) {
+            tree.put(element);
+        }
+        tree.remove(4);
+        assertThat(tree.inSymmetricalOrder()).hasSize(0)
+                .containsExactly();
+    }
+
+    @Test
+    void whenRemoveRoot2ChildsWithChildsThenOk() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.put(element);
+        }
+        tree.remove(4);
+        assertThat(tree.inSymmetricalOrder()).hasSize(6)
+                .containsExactly(1, 2, 3, 5, 6, 7);
+    }
+
+    @Test
+    void whenRemoveNotChildThenOk() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.put(element);
+        }
+        tree.remove(3);
+        assertThat(tree.inSymmetricalOrder()).hasSize(6)
+                .containsExactly(1, 2, 4, 5, 6, 7);
+    }
+
+    @Test
+    void whenRemove2ChildWithoutChildsThenOk() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int element : new int[]{4, 2, 6, 3, 5, 7, 1}) {
+            tree.put(element);
+        }
+        tree.remove(2);
+        assertThat(tree.inSymmetricalOrder()).hasSize(6)
+                .containsExactly(1, 3, 4, 5, 6, 7);
+    }
+
+    @Test
+    void whenRemove2ChildWithChildsThenOk() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        for (int element : new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7}) {
+            tree.put(element);
+        }
+        tree.remove(10);
+        assertThat(tree.inSymmetricalOrder()).hasSize(16)
+                .containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17);
+    }
 }
