7ffe8dcb6c20863912ad58281a4d765dd29cf804
olga.ilyina <olga.ilyina@axenix.pro>
2025-01-17 16:31:46 +0300

    Бинарное дерево поиска. Удаление узла. [#504985 #528927]

--- src/main/java/ru/job4j/collection/binarytree/Main.java
+++ src/main/java/ru/job4j/collection/binarytree/Main.java
@@ -7,7 +7,6 @@ public class Main {
         System.out.println(exampleTree);
         */
 
-        /*
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
         int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
         for (int i : array) {
@@ -17,18 +16,5 @@ public class Main {
         System.out.println(bst.remove(10));
         System.out.println("После удаления узла 10 :");
         System.out.println(bst);
-        */
-
-        ///*
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        int[] array = new int[]{4, 2, 6, 3, 5, 7, 1};
-        for (int i : array) {
-            bst.put(i);
-        }
-        System.out.println(bst);
-        System.out.println(bst.remove(2));
-        System.out.println("После удаления узла 2:");
-        System.out.println(bst);
-       //*/
     }
 }
