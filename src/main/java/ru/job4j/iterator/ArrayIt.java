136d82bfc80b95838087e0f69334795d5b730b87
olga.ilyina <olga.ilyina@axenix.pro>
2025-01-21 17:41:07 +0300

    Фиксация изменений

--- src/main/java/ru/job4j/iterator/ArrayIt.java
+++ src/main/java/ru/job4j/iterator/ArrayIt.java
@@ -7,16 +7,19 @@ public class ArrayIt implements Iterator<Integer> {
     private int point = 0;
 
     public ArrayIt(int[] data) {
+
         this.data = data;
     }
 
     @Override
     public boolean hasNext() {
+
         return point < data.length;
     }
 
     @Override
     public Integer next() {
+
         return data[point++];
     }
 }
